47117c355b7b20e27a8941fcba234d8170bbb361
thanni <thanni073664@outlook.com>
2020-05-09 09:50:34 +0100

    mvc controller added

---
+++ Learn.BookStore/Controller/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Learn.BookStore
+{
+    public class HomeController: Controller
+    {
+        public string Index()
+        {
+            return "Welcome to ASP.NET Core Class";
+        }
+    }
+}
